42d8283f82eb9f9830b7380dcf3cf1fe1a34804d
Aaron Harvey <10888871+aaron-harvey@users.noreply.github.com>
2019-08-06 18:38:36 -0400

    fix: config must be an object error

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname)));
 
-metadata(app)(config.passport.saml);
+metadata(config.passport.saml).bind(app)();
 
 require('./config/routes')(app, config, passport);
 
